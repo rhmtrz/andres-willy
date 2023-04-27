95fbf5cfd06345e2b9e8287ce1facbeb4663ad6f
rhmtrz <rezaeihazara@gmail.com>
2023-04-28 00:01:52 +0900

    chore: clean up code

--- src/willy.ino
+++ src/willy.ino
@@ -5,10 +5,6 @@
 AtomWilly myServo(1, 400, 2500);
 AtomWilly myMotor(3, 400, 2500);
 
-#define RING_BUFFER_LENGTH   20       // 最大1パケットサイズ以上を指定
-#define MIN_FRAME_LENGTH   3        // 最小フレーム長を指定
-#define FRAME_SOF          0xff     // 先頭バイト0xFF
-
 #define RED          0xff0000 
 #define GREEN          0x00ff00 
 #define BLUE          0x0000ff 
@@ -16,12 +12,12 @@ AtomWilly myMotor(3, 400, 2500);
 void setup() {
   // hardware initialization
   M5.begin(true, false, true);
-  M5.dis.drawpix(0, 0x0000ff);
+  M5.dis.drawpix(0, GREEN);
   PS4.begin("78:25:db:55:48:a8");  // "78:21:84:93:d8:48"
   myServo.attach(21);
   myMotor.attach(22);
 
-  M5.dis.drawpix(0, 0xff0000);
+  M5.dis.drawpix(0, RED);
 }
 
 void loop() {
@@ -36,11 +32,11 @@ void loop() {
 
     if (ly > 100) {
       myMotor.write(ly);
-      M5.dis.drawpix(0, 0x0000ff);
+      M5.dis.drawpix(0, BLUE);
     } 
     if (ly < 100) {
       myMotor.write(ly);
-      M5.dis.drawpix(0, 0xff0000);
+      M5.dis.drawpix(0, RED);
     }
     if (PS4.Down()) {
       myMotor.write(97);
@@ -48,10 +44,10 @@ void loop() {
 
     if (rx < 90) {
       myServo.write(rx);
-      M5.dis.drawpix(0, 0xff0000);
+      M5.dis.drawpix(0, RED);
     } else if (rx > 90) {
       myServo.write(rx);
-      M5.dis.drawpix(0, 0x0000ff);
+      M5.dis.drawpix(0, BLUE);
     } else {
       myServo.write(90);
     }
